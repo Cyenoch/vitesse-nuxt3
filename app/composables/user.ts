1850ded00a4e092331abfe4ead710cb8b9a8a4ad
Cyenoch <cyenoch@outlook.com>
2024-11-25 17:47:45 +0800

    chore: Update deps

--- app/composables/user.ts
+++ app/composables/user.ts
@@ -1,5 +1,9 @@
 import { acceptHMRUpdate, defineStore } from 'pinia'
 
+export const authTokenStorage = useLocalStorage<string | null>('token', null, { writeDefaults: true })
+
+export const authToken = computed(() => authTokenStorage.value)
+
 export const useUserStore = defineStore('user', () => {
   return {
   }
--- app/layouts/example.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <main class="p-4 space-y-2">
-    <slot />
-    <AppFooter />
-  </main>
-</template>
